03a833d792f1c116ef8828643db67322cd0c5de0
tinotendnyashanu <tinotendanyash@gmail.com>
2023-11-17 07:33:51 +0100

    init

---
+++ components/footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2023 Store, Inc. All rights reserved{" "}
+          <a
+            href="https://www.tinotendanyashanu.pl/"
+            className=" text-blue-500"
+          >
+            Tinotenda Nyashanu
+          </a>
+          .
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
